4441661c2c545083591a74210e51f182307190b1
Sohail <sohailshaikh07899@gmail.com>
2019-09-19 02:38:29 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      taskName : "",
+      list : []
+    };
+  }
+
+  addItem(taskValue){
+    if(taskValue !== ""){
+      const newItem = {
+        id: Date.now(),
+        value: taskValue,
+        isDone:false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        taskName: ""
+      });
+    }
+  }
+
+  removeItem(id){
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
+  updateInput(input){
+    this.setState({ newItem:input });
+  }
+
+  render(){
+    return(
+      <div className="App-container">
+        <h1 className="App-header">Todo App</h1>
+        <br />
+        <input 
+          onChange={e => this.updateInput(e.target.value)}  
+          value={this.state.newItem} 
+          type="text" 
+          placeholder="Enter Task Name" 
+          required />
+        <button
+          onClick={() => this.addItem(this.state.newItem)} 
+          className="btn-add"
+        >Add</button><br />
+        <h3>Task List:</h3>
+        <div className="list">
+          <ul>
+            {
+              this.state.list.map(item => {
+                return(
+                    <li key={item.id}>
+                      <input
+                        onChange={() => {}} 
+                        type="checkbox"
+                        checked={item.isDone} />
+    
+                        {item.value}
+    
+                      <button
+                      className="btn-delete"
+                        onClick={() => this.removeItem(item.id)}
+                      >Delete</button>
+                    </li>
+                );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
