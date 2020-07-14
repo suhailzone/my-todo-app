ade8f6295f57b32130b40e7d595cc2321b635e6a
Sohail Shaikh <sohailshaikh07899@gmail.com>
2020-07-15 00:17:11 +0530

    Updates

---
+++ precache-manifest.75e58d627e1c7d6f39a651e0d564f842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff555579c9d542086339ec1b95d97ea",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "3b1215537a5c34b9995e",
+    "url": "/todoapp/static/css/main.87aef55a.chunk.css"
+  },
+  {
+    "revision": "44f6c5a8099780da879c",
+    "url": "/todoapp/static/js/2.cd6b7546.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/todoapp/static/js/2.cd6b7546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1215537a5c34b9995e",
+    "url": "/todoapp/static/js/main.6ba6a4a0.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
